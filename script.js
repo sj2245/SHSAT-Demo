28f50b6907fa06f3f8c962fea3d463b3f16dc845
sj2245 <shakespearejamesii@gmail.com>
2023-07-30 16:35:39 -0400

    wowzer added da linkz!!!

--- script.js
+++ script.js
@@ -11,5 +11,19 @@ let mobilemenu = getdoc(`header nav ul`);
 
 mobilemenutrigger.addEventListener(`click`, function (e) {
   print({mobilemenutrigger, mobilemenu});
-  mobilemenu.classList.toggle(`collapsed`)
+  mobilemenu.classList.toggle(`collapsed`);
 })
+
+function buttonlink(button, link) {
+  button.addEventListener(`click`, function (e) {
+    window.location.href = link;
+  })
+}
+
+let practiceButton = getdoc(`.practiceButton`);
+let aboutButton = getdoc(`.aboutButton`);
+let updatesButton = getdoc(`.updatesButton`);
+
+buttonlink(aboutButton, `./pages/about.html`);
+buttonlink(updatesButton, `./pages/updates.html`);
+buttonlink(practiceButton, `./pages/questions.html`);
